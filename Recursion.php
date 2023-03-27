f2d3cb7a5c460a61d7e270bdefa5a1b2898db493
Viacheslav <zhizhkoslava@gmail.com>
2023-03-27 19:15:59 +0300

    fix resolution

--- Recursion.php
+++ Recursion.php
@@ -2,14 +2,12 @@
 
 function fact($number)
 {
-    if ($number <= 1)
-    {
-        return 1;
+    if ($number <= 1) 
+	{ 
+	 return 1; 
     }
-    else
-    {
-        return $number * fact($number - 1);
-    }
+ 
+    return $number * fact($number - 1);    
 }
 
 echo fact(10);
