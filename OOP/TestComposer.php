be325d660fc00c170a80c7e13987eadc3b0c512a
Viacheslav <zhizhkoslava@gmail.com>
2023-05-18 15:44:39 +0300

    added new packagist zhyzhkoslava/simple-laravel-dumper repo

--- OOP/TestComposer.php
+++ OOP/TestComposer.php
@@ -2,7 +2,10 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$dd = new \Zhyzhkoslava\SimpleLaravelDumper\DD();
+
 $client = new \GuzzleHttp\Client();
+$dd->dd($client);
 $response = $client->request('GET', 'https://itea.ua');
 
 if ($response->getStatusCode() == 200){
